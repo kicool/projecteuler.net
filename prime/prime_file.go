626d3779d2d65c4f086bacb427e489a6d016550b
Kicool <kicool@foxmail.com>
2012-05-22 10:15:33 +0800

    add tracing and profiling, add 2 another way to get numbers from file

--- prime/prime_file.go
+++ prime/prime_file.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"errors"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
+	_ "net/http/pprof"
+	"os"
 	"regexp"
+	"runtime/pprof"
 	"sort"
 	"strconv"
-	"flag"
+	"time"
 )
 
 const (
@@ -18,8 +22,26 @@ const (
 )
 
 var (
-	path = flag.String("path", "./data/10000.prime", "open path prime store")
+	path       = flag.String("path", f1M, "open path prime store")
+	cpuprofile = flag.String("cpuprofile", "", "write cpu profile to file")
 )
+
+type trace struct {
+	s string
+	t time.Time
+}
+
+func NewTrace(s string) (t *trace) {
+	log.Println("-->:", s)
+	t = new(trace)
+	t.s = s
+	t.t = time.Now()
+	return
+}
+
+func Tracer(t *trace) {
+	log.Println("<--:", t.s, time.Since(t.t))
+}
 
 type PrimeStore struct {
 	path  string
@@ -34,6 +56,8 @@ type PrimeStore struct {
 }
 
 func NewPrimeStore(file string) (ps *PrimeStore, err error) {
+	defer Tracer(NewTrace("NewPrimeStore"))
+
 	ps = new(PrimeStore)
 	ps.path = file
 	err = ps.loadFromFile()
@@ -55,7 +79,77 @@ func NewPrimeStore(file string) (ps *PrimeStore, err error) {
 	return
 }
 
+func NewPrimeStore2(file string) (ps *PrimeStore, err error) {
+	defer Tracer(NewTrace("NewPrimeStore2"))
+
+	ps = new(PrimeStore)
+	ps.path = file
+
+	buf, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Printf("Open file.%s error.%s", file, err)
+		return nil, err
+	}
+
+	c := 1000001
+	ps.index = make([]uint64, c, c)
+	ps.count = 0
+	for i := uint64(0); i < uint64(len(buf)); i++ {
+		v, j := nextUint64(buf, i)
+		if i != j {
+			ps.index[ps.count] = v
+			ps.count++
+		}
+		i = j
+	}
+	ps.count--
+	ps.index = ps.index[:ps.count]
+
+	ps.itoa()
+
+	ps.first = ps.index[0]
+	ps.last = ps.index[ps.count-1]
+	ps.low = 1
+	ps.high = ps.last
+
+	return
+}
+
+func NewPrimeStore3(file string) (ps *PrimeStore, err error) {
+	defer Tracer(NewTrace("NewPrimeStore3"))
+
+	ps = new(PrimeStore)
+	ps.path = file
+
+	buf, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Printf("Open file.%s error.%s", file, err)
+		return nil, err
+	}
+
+	for i := uint64(0); i < uint64(len(buf)); i++ {
+		v, j := nextUint64(buf, i)
+		if i != j {
+			ps.index = append(ps.index, v)
+		}
+		i = j
+	}
+
+	ps.count = uint64(len(ps.index))
+
+	ps.itoa()
+
+	ps.first = ps.index[0]
+	ps.last = ps.index[ps.count-1]
+	ps.low = 1
+	ps.high = ps.last
+
+	return
+}
+
 func (ps *PrimeStore) IsPrime(n uint64) (bool, error) {
+	defer Tracer(NewTrace("IsPrime"))
+
 	if n < ps.low || n > ps.high {
 		return false, errors.New("out of range")
 	}
@@ -80,19 +174,21 @@ GetByIndex(3) return 5
 OutOfRange in Store as return 0
 */
 func (ps *PrimeStore) GetByIndex(nth uint64) (n uint64) {
+	defer Tracer(NewTrace("GetByIndex"))
+
 	n = 0
 	if nth < ps.base || nth >= (ps.base+ps.count) {
 		log.Print("out of range.", nth, " ", ps)
 		return
 	}
 
-	n, _ = strconv.ParseUint(ps.store[nth-ps.base], 0, 64)
+	n = ps.index[nth-ps.base]
 	return
 }
 
 //No zero
 func isPan(n string, nozero bool) bool {
-	bitmap := []int{0,0,0,0,0,0,0,0,0,0}
+	bitmap := []int{0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
 	for _, v := range n {
 		bitmap[v-'0']++
 		if bitmap[v-'0'] > 1 {
@@ -102,8 +198,8 @@ func isPan(n string, nozero bool) bool {
 			return false
 		}
 	}
-	for i:=1; i <=8; i++ {
-		if bitmap[i]==0 && bitmap[i+1]==1 {
+	for i := 1; i <= 8; i++ {
+		if bitmap[i] == 0 && bitmap[i+1] == 1 {
 			return false
 		}
 	}
@@ -111,7 +207,9 @@ func isPan(n string, nozero bool) bool {
 }
 
 func (ps *PrimeStore) GetPandigitalLargest() (n uint64) {
-	for i := ps.count-1; i >= ps.base; i-- {
+	defer Tracer(NewTrace("GetPandigitalLargest"))
+
+	for i := ps.count - 1; i >= ps.base; i-- {
 		if isPan(ps.store[i], true) {
 			return ps.index[i]
 		}
@@ -123,7 +221,23 @@ func (ps *PrimeStore) String() string {
 	return fmt.Sprint("bsae,count,first,last,low,high=", ps.base, ps.count, ps.first, ps.last, ps.low, ps.high)
 }
 
+func isDigit(b byte) bool {
+	return '0' <= b && b <= '9'
+}
+
+func nextUint64(b []byte, i uint64) (uint64, uint64) {
+	for ; i < uint64(len(b)) && !isDigit(b[i]); i++ {
+	}
+	x := uint64(0)
+	for ; i < uint64(len(b)) && isDigit(b[i]); i++ {
+		x = x*10 + uint64(b[i]) - '0'
+	}
+	return x, i
+}
+
 func (ps *PrimeStore) loadFromFile() (err error) {
+	defer Tracer(NewTrace("Parse by regexp"))
+
 	file, err := ioutil.ReadFile(ps.path)
 	if err != nil {
 		log.Printf("Open file.%s error.%s", ps.path, err)
@@ -146,9 +260,20 @@ func (ps *PrimeStore) dump() {
 }
 
 func (ps *PrimeStore) atoi() {
+	defer Tracer(NewTrace("atoi"))
+
 	ps.index = make([]uint64, ps.count, ps.count)
 	for i, v := range ps.store {
 		ps.index[i], _ = strconv.ParseUint(v, 0, 64)
+	}
+}
+
+func (ps *PrimeStore) itoa() {
+	defer Tracer(NewTrace("itoa"))
+
+	ps.store = make([]string, ps.count, ps.count)
+	for i, v := range ps.index {
+		ps.store[i] = strconv.FormatUint(v, 10)
 	}
 }
 
@@ -171,26 +296,47 @@ func (index Uint64Slice) Swap(i, j int) {
 }
 
 func main() {
+	defer Tracer(NewTrace("main"))
+
 	flag.Parse()
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		pprof.StartCPUProfile(f)
+		defer pprof.StopCPUProfile()
+	}
 
 	ps, err := NewPrimeStore(*path)
 	if err != nil {
 		return
 	}
+	ps2, err := NewPrimeStore2(*path)
+	if err != nil {
+		return
+	}
+	ps3, err := NewPrimeStore3(*path)
+	if err != nil {
+		return
+	}
 	/*
-	fmt.Println(ps)
+		fmt.Println(ps)
 
-	fmt.Println(ps.GetByIndex(0))
-	fmt.Println(ps.GetByIndex(10))
-	fmt.Println(ps.GetByIndex(1000))
-	fmt.Println(ps.GetByIndex(10000))
-	fmt.Println(ps.IsPrime(1))
-	fmt.Println(ps.IsPrime(2))
-	fmt.Println(ps.IsPrime(3))
-	fmt.Println(ps.IsPrime(4))
-	fmt.Println(ps.IsPrime(100))
-	fmt.Println(ps.IsPrime(101))
+		fmt.Println(ps.GetByIndex(0))
+		fmt.Println(ps.GetByIndex(10))
+		fmt.Println(ps.GetByIndex(1000))
+		fmt.Println(ps.GetByIndex(10000))
+		fmt.Println(ps.IsPrime(1))
+		fmt.Println(ps.IsPrime(2))
+		fmt.Println(ps.IsPrime(3))
+		fmt.Println(ps.IsPrime(4))
+		fmt.Println(ps.IsPrime(100))
+		fmt.Println(ps.IsPrime(101))
 	*/
 
 	fmt.Println(ps.GetPandigitalLargest())
+	fmt.Println(ps2.GetPandigitalLargest())
+	fmt.Println(ps3.GetPandigitalLargest())
 }
